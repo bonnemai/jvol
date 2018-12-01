caad306f845504022d010f0e51f6fcf532ed307a
bonnemai <bonnemai@yahoo.fr>
2018-12-01 17:14:26 +0800

    JFile: add surface.

--- jfile.py
+++ jfile.py
@@ -40,17 +40,22 @@ class JFile():
         abs_expiries=[]
         rel_strikes=[]
         rel_expiries=[]
-
+        abs_vol_surface=[]
         while index_sheet.cell_value( 2, i_strike)!='':
             abs_strikes.append(index_sheet.cell_value( 2, i_strike))
             i_strike+=1
         while index_sheet.cell_value( i_abs_expiries,0 )!='':
             abs_expiries.append(index_sheet.cell_value( i_abs_expiries, 0))
+            vol_line=[]
+            for i in range(1,i_strike):
+                vol_line.append(index_sheet.cell_value(i_abs_expiries, i))
             i_abs_expiries+=1
+            abs_vol_surface.append(vol_line)
 
         i_strike=2
         i_abs_expiries+=1
         i_rel_expiries=1
+
         while index_sheet.cell_value( i_abs_expiries, i_strike)!='':
             rel_strikes.append(index_sheet.cell_value( i_abs_expiries, i_strike))
             i_strike+=1
@@ -62,6 +67,7 @@ class JFile():
         self.results[index_name]={'spot': spot,
                                   'abs_strikes': abs_strikes,
                                   'abs_expiries': abs_expiries,
+                                  'abs_vol_surface':abs_vol_surface,
                                   'rel_strikes': rel_strikes,
                                   'rel_expiries': rel_expiries}
 

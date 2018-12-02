4895ba14c30e6f9b46a2e417e84ed5444902c54a
bonnemai <bonnemai@yahoo.fr>
2018-12-02 18:21:48 +0800

    Add a test.

--- jfile.py
+++ jfile.py
@@ -5,7 +5,6 @@ import logging
 
 logger=logging.getLogger(__name__)
 
-
 def has_cell(index_sheet, i, j):
     try:
         return index_sheet.cell_value(i, j)!=''
@@ -19,64 +18,70 @@ class JException(Exception):
 
 
 class JFile():
-    def __init__(self, is_today=True, region='APAC', date_str=None, path=r'./resources/J_Vol.xlsx'):
+    def __init__(self, is_today=True, region='APAC', date_str=None, path=r'./resources/J_Vol.xlsx', indices=['NKY', 'KOSPI2', 'HSCEI', 'HSI']):
         if path is None:
             if region=='DEV':
                 path=r'./test/resources/J_Vol.xlsx'
         self.results={}
         self.comments=[]
         # TODO: find files, get the latest, add a comment...
-        index_name="NKY"
+
         workbook=open_workbook(path)
-        print(workbook.sheet_names())
-        index_sheet=workbook.sheet_by_name(index_name)
-        spot=index_sheet.cell(1, 2).value
-        if spot is None or spot==0:
-            raise JException('No Spot found for %s'.format(index_name))
+        logger.info('Sheets found: %s', workbook.sheet_names())
+        for index_name in indices:
+            index_sheet=workbook.sheet_by_name(index_name)
+            spot=index_sheet.cell(1, 2).value
+            if spot is None or spot==0:
+                raise JException('No Spot found for %s'.format(index_name))
 
-        i_strike=2
-        i_abs_expiries=3
-        abs_strikes=[]
-        abs_expiries=[]
-        rel_strikes=[]
-        rel_expiries=[]
-        abs_vol_surface=[]
-        while index_sheet.cell_value( 2, i_strike)!='':
-            abs_strikes.append(index_sheet.cell_value( 2, i_strike))
-            i_strike+=1
-        while index_sheet.cell_value( i_abs_expiries,0 )!='':
-            abs_expiries.append(index_sheet.cell_value( i_abs_expiries, 0))
-            vol_line=[]
-            for i in range(1,i_strike):
-                vol_line.append(index_sheet.cell_value(i_abs_expiries, i))
+            i_strike=2
+            i_abs_expiries=3
+            abs_strikes=[]
+            abs_expiries=[]
+            rel_strikes=[]
+            rel_expiries=[]
+            abs_vol_surface=[]
+            while index_sheet.cell_value( 2, i_strike)!='':
+                abs_strikes.append(index_sheet.cell_value( 2, i_strike))
+                i_strike+=1
+            while index_sheet.cell_value( i_abs_expiries,0 )!='':
+                abs_expiries.append(index_sheet.cell_value( i_abs_expiries, 0))
+                vol_line=[]
+                for i in range(1,i_strike):
+                    vol_line.append(index_sheet.cell_value(i_abs_expiries, i))
+                i_abs_expiries+=1
+                abs_vol_surface.append(vol_line)
+
+            i_strike=2
             i_abs_expiries+=1
-            abs_vol_surface.append(vol_line)
-
-        i_strike=2
-        i_abs_expiries+=1
-        # i_rel_expiries=1
-        rel_vol_surface=[]
-        while index_sheet.cell_value( i_abs_expiries, i_strike)!='':
-            rel_strikes.append(index_sheet.cell_value( i_abs_expiries, i_strike))
-            i_strike+=1
-        i_abs_expiries+=1
-        while has_cell( index_sheet, i_abs_expiries,0 ):
-            rel_expiries.append(index_sheet.cell_value( i_abs_expiries, 0))
-            vol_line=[]
-            # if has_cell( index_sheet, i_abs_expiries,0 ):
-            for i in range(1,i_strike):
-                vol_line.append(index_sheet.cell_value(i_abs_expiries, i))
-            rel_vol_surface.append(vol_line)
+            # i_rel_expiries=1
+            rel_vol_surface=[]
+            while index_sheet.cell_value( i_abs_expiries, i_strike)!='':
+                rel_strikes.append(index_sheet.cell_value( i_abs_expiries, i_strike))
+                i_strike+=1
             i_abs_expiries+=1
+            while has_cell( index_sheet, i_abs_expiries,0 ):
+                rel_expiries.append(index_sheet.cell_value( i_abs_expiries, 0))
+                vol_line=[]
+                # if has_cell( index_sheet, i_abs_expiries,0 ):
+                for i in range(1,i_strike):
+                    vol_line.append(index_sheet.cell_value(i_abs_expiries, i))
+                rel_vol_surface.append(vol_line)
+                i_abs_expiries+=1
 
-        self.results[index_name]={'spot': spot,
-                                  'abs_strikes': abs_strikes,
-                                  'abs_expiries': abs_expiries,
-                                  'abs_vol_surface':abs_vol_surface,
-                                  'rel_strikes': rel_strikes,
-                                  'rel_expiries': rel_expiries,
-                                  'rel_vol_surface':rel_vol_surface}
+            self.results[index_name]={'spot': spot,
+                                      'absolute': {'strikes': abs_strikes,
+                                      'expiries': abs_expiries,
+                                      'vol_surface':abs_vol_surface},
+                                      'relative': {'strikes': rel_strikes,
+                                      'expiries': rel_expiries,
+                                      'vol_surface':rel_vol_surface}}
 
+    def get_vol_value(self, strike, expiry, moneyness='absolute', index_name='NKY'):
+        vol_surface=self.results[index_name][moneyness]
+        i_expiry=vol_surface['expiries'].index(expiry)
+        i_strike=vol_surface['strikes'].index(strike)
+        return vol_surface['vol_surface'][i_expiry][i_strike]
 
     def add_comment(self, comment, criticity='info'):
         # print(self.comments.a)
--- test/test_jfile.py
+++ test/test_jfile.py
@@ -5,7 +5,12 @@ logger=logging.getLogger(__name__)
 
 class JFileTest(unittest.TestCase):
     def test_jfile1(self):
-        jf=JFile(region='DEV')
+        jf=JFile(region='DEV', indices=['NKY'])
         self.assertIsNotNone(jf)
         logger.info(jf.results['NKY'])
         print(jf.results['NKY'])
+
+        vol_value=jf.get_vol_value(strike=12000, expiry='7Dec18')
+        logger.info(vol_value)
+        self.assertIsNotNone(vol_value)
+        self.assertEqual(10, vol_value, 'Should be equal to 10')
